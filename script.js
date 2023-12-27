919085a7460af782984ff68a2eef9a00487b79aa
gopikaa09 <gopika.guduru@gmail.com>
2023-12-27 13:13:52 +0530

    changes

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const togglenavbar = () => {
   console.log("hello");
   if (navhidden.style.display == "flex") {
     navhidden.style.display = "none";
-    // navhidden.style.flexDirection = "row";
   } else {
     navhidden.style.display = "flex";
     navhidden.style.flexDirection = "column";
+    navhidden.style.alignItems = "center";
+    navhidden.style.width = "100%";
   }
 };
